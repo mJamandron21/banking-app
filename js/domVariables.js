7842e7db6a9367249f12ac51f2cc19164fd7ce8c
Mark Kenneth Jamandron <mjamandron@gmail.com>
2021-12-19 11:02:16 +0800

    Login form/page Added

--- js/domVariables.js
+++ js/domVariables.js
@@ -5,6 +5,10 @@ export const navCollapseDiv = document.querySelector('.navbar-collapse');
 //login button-navbar first page
 export const navLoginBtn = document.querySelector('.btn-start');
 
+//submit button on login page
+export const submitLogin = document.querySelector('.submit-login');
+
 //pages
 export const firstPage = document.querySelector('.first');
 export const secondPage = document.querySelector('.second');
+export const thirdPage = document.querySelector('.third');
