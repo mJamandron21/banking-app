31ad3a0d9074664097e380e1a31759f081f0fdfd
Mark Kenneth Jamandron <mjamandron@gmail.com>
2022-01-06 19:38:00 +0800

    Update: ifCurrentUserNotExist added!

--- js/client.js
+++ js/client.js
@@ -1,6 +1,7 @@
 import { logout } from "./logoutScript.js";
-import {displayClientDetails} from "./clientDetails.js"
+import {displayClientDetails, ifCurrentUserNotExist} from "./clientDetails.js"
 
 //functions call outside
 logout();
+ifCurrentUserNotExist();
 displayClientDetails();
--- js/clientDetails.js
+++ js/clientDetails.js
@@ -5,6 +5,15 @@ import {
 } from "./domVariables.js";
 
 
+export function ifCurrentUserNotExist(){
+    let current_user = new Array();
+    current_user = JSON.parse(localStorage.getItem("currentUser"))?JSON.parse(localStorage.getItem("currentUser")):[] 
+
+    if(current_user == ''){
+        location.replace("./index.html");
+    }
+}
+
 export function displayClientDetails(){
     
     let current_user = new Array();
